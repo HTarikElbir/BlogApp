7516dc498d9da213b8c100e371e7d121b8ad14a7
HTarık <244035008@ogr.cbu.edu.tr>
2025-03-25 09:52:34 +0300

    feat: Add InitialCreate Migration

--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -4,9 +4,12 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<BlogContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
-builder.Services.AddDbContext<BlogContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnetion")));
+
 
 app.MapGet("/", () => "Hello World!");
 
