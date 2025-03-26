59e8b99f4a980851cd62bc0f6d9bd960fe470d11
HTarık <244035008@ogr.cbu.edu.tr>
2025-03-26 23:39:43 +0300

    chore: Change database configuration from MySQL to SQLite

--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -5,12 +5,13 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<BlogContext>(options =>
-//options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
-{
-var connectionString = builder.Configuration.GetConnectionString("MySqlConnection");
-var version = new MySqlServerVersion(new Version(8, 0, 30));
-    options.UseMySql(connectionString, version);
-});
+options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+//{
+//var connectionString = builder.Configuration.GetConnectionString("MySqlConnection");
+//var version = new MySqlServerVersion(new Version(8, 0, 30));
+//    options.UseMySql(connectionString, version);
+//}
+);
 
 var app = builder.Build();
 
