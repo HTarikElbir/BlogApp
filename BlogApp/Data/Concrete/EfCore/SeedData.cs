a9d3453d80a9f57ff34d69ed6cf002a952efcdf4
HTarık <244035008@ogr.cbu.edu.tr>
2025-03-28 04:13:25 +0300

    feat: Add templates

--- BlogApp/Data/Concrete/EfCore/SeedData.cs
+++ BlogApp/Data/Concrete/EfCore/SeedData.cs
@@ -56,6 +56,7 @@ namespace BlogApp.Data.Concrete.EfCore
                             IsActive = true,
                             PublishedOn = DateTime.Now.AddDays(-10),
                             Tags = context.Tags.Take(3).ToList(),
+                            Image = "1.jpg",
                             UserId = 1
                         },
                         new Post
@@ -65,6 +66,7 @@ namespace BlogApp.Data.Concrete.EfCore
                             IsActive = true,
                             PublishedOn = DateTime.Now.AddDays(-5),
                             Tags = context.Tags.Skip(2).Take(2).ToList(),
+                            Image = "2.jpg",
                             UserId = 2
                         },
                         new Post
@@ -74,6 +76,7 @@ namespace BlogApp.Data.Concrete.EfCore
                             IsActive = true,
                             PublishedOn = DateTime.Now.AddDays(-3),
                             Tags = context.Tags.Skip(1).Take(2).ToList(),
+                            Image = "3.jpg",
                             UserId = 1
                         }
                     );
